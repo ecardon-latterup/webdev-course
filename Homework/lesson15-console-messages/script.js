f6ae5f7b01b62ea3f6cd93052078ac26445e81fe
E Cardon <erikacardon@latterup.org>
2026-06-29 15:50:12 -0500

    lesson 15 complete

---
+++ Homework/lesson15-console-messages/script.js
@@ -0,0 +1,8 @@
+const firstName = "Alex"
+let age = 17
+const hobby1 = "skateboard"
+const hobby2 = "photography"
+const year = 2050
+
+console.log("Hey there, fellow developers! My name is", firstName, "and I'm", age, "years old. In addition to coding, I love to", hobby1, "and", hobby2, ". I'm hoping to master a heel flip by", year, "on a hover skateboard!")
+console.log(`Hey there, fellow developers! My name is ${firstName} and I'm ${age} years old. In addition to coding, I love to ${hobby1} and ${hobby2}. I'm hoping to master a heel flip by ${year} on a hover skateboard!`)
---
+++ InClassActivities/lesson-15/script.js
@@ -0,0 +1,7 @@
+const firstName = "Erika"
+const favColor = "green"
+let luckyNumber = 14
+
+console.log("First name: ", firstName)
+console.log("Favorite color: ", favColor)
+console.log("Lucky number: ", luckyNumber)
